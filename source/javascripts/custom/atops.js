8a2e3e590b015d44cda6c75948f36687a0ef3ad1
kjellski <otto.kjell@gmail.com>
2013-07-01 23:00:09 +0200

    added loader gif, started fiddling with jquery promises to get the idea, added a github promise to be used for fetching github repos, not used for rendering yet...

--- source/javascripts/custom/atops.js
+++ source/javascripts/custom/atops.js
@@ -115,6 +115,8 @@ var createBindingWithToggleState = function (selector, data) {
     atops.push(atop);
 }
 
+$('#atop-content').hide();
+
 var lipsum = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor " +
 "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation " + 
 "ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit " + 
@@ -125,9 +127,9 @@ var github_data = {
     name: 'github'
     , image: '/images/social/Github.png'
     , items: [
-        { title: 'testtitle1', description: lipsum }
-      , { title: 'testtitle1', description: lipsum }
-      , { title: 'testtitle1', description: lipsum }
+    { title: 'testtitle1', description: lipsum }
+    , { title: 'testtitle1', description: lipsum }
+    , { title: 'testtitle1', description: lipsum }
     ]
 }
 
@@ -135,9 +137,9 @@ var twitter_data = {
     name: 'twitter'
     , image: '/images/social/Twitter.png'
     , items: [
-        { title: 'testtitle1', description: lipsum }
-      , { title: 'testtitle1', description: lipsum }
-      , { title: 'testtitle1', description: lipsum }
+    { title: 'testtitle1', description: lipsum }
+    , { title: 'testtitle1', description: lipsum }
+    , { title: 'testtitle1', description: lipsum }
     ]
 }
 
@@ -145,49 +147,109 @@ var coderwall_data = {
     name: 'coderwall'
     , image: '/images/social/Coderwall.png'
     , items: [
-        { title: 'testtitle1', description: lipsum }
-      , { title: 'testtitle1', description: lipsum }
-      , { title: 'testtitle1', description: lipsum }
+    { title: 'testtitle1', description: lipsum }
+    , { title: 'testtitle1', description: lipsum }
+    , { title: 'testtitle1', description: lipsum }
     ]
 }
 
-bindElementToAtopOpening('#github-social', github_data);
-bindElementToAtopOpening('#twitter-social', twitter_data);
-bindElementToAtopOpening('#coderwall-social', coderwall_data);
+var github = {
+    showRepos: function(options) {
+        this.options = options;
+        this.deferred = new $.Deferred();
+        this._getData();
+        return this.deferred.promise();
+    },
+
+    _getData: function(deferred) {
+        $.getJSON("https://api.github.com/users/"+this.options.user+"/repos?callback=?", $.proxy(this._getDataCallback, this));
+    },
+
+    _getDataCallback: function(data) {
+        var repos = [];
+        if (!data || !data.data) { return; }
+        for (var i = 0; i < data.data.length; i++) {
+            if (this.options.skip_forks && data.data[i].fork) { continue; }
+            repos.push(data.data[i]);
+        }
+        repos.sort(function(a, b) {
+            var aDate = new Date(a.pushed_at).valueOf(),
+            bDate = new Date(b.pushed_at).valueOf();
+
+            if (aDate === bDate) { return 0; }
+            return aDate > bDate ? -1 : 1;
+        });
+
+        if (this.options.count) { repos.splice(this.options.count); }
+        this.data = this._render(repos);
+        this.deferred.resolve('finished fetching github data');
+    },
+
+    _render: function(repos) {
+        var data = {
+            name: 'github'
+            , image: '/images/social/Github.png'
+            , items: []
+        }
+
+        for(var i = 0; i < repos.length && i < 5; i++) {
+            data.items.push({ title: repos[i].name, description: repos[i].description});
+        }
+
+        return data;
+    }
+};
+
+// if(githubInfo.data('github-user')) {
+    var githubPromise = github.showRepos({
+        user: 'kjellski',
+        count: 10,
+        skip_forks: true
+    });
+
+    $.when(githubPromise).then(function (msg) {
+      console.log(msg);
+      console.log(github.data);
+    });
+
+    // bindElementToAtopOpening('#github-social', promise);
+// }
+// bindElementToAtopOpening('#twitter-social', twitter_data);
+// bindElementToAtopOpening('#coderwall-social', coderwall_data);
 
 // // all credits go here: http://web.enavu.com/demos/carousel.html
 // $(document).ready(function() {
 //     //move he last list item before the first item. The purpose of this is if the user clicks to slide left he will be able to see the last item.
 //     $('#carousel_ul li:first').before($('#carousel_ul li:last')); 
-    
+
 //     //when user clicks the image for sliding right        
 //     $('#right_scroll img').click(function(){
 
 //         //get the width of the items ( i like making the jquery part dynamic, so if you change the width in the css you won't have o change it here too ) '
 //         var item_width = $('#carousel_ul li').outerWidth() + 10;
-        
+
 //         //calculae the new left indent of the unordered list
 //         var left_indent = parseInt($('#carousel_ul').css('left')) - item_width;
-        
+
 //         //make the sliding effect using jquery's anumate function '
 //         $('#carousel_ul:not(:animated)').animate({'left' : left_indent},500,function(){    
 
 //             //get the first list item and put it after the last list item (that's how the infinite effects is made) '
 //             $('#carousel_ul li:last').after($('#carousel_ul li:first')); 
-            
+
 //             //and get the left indent to the default -210px
 //             $('#carousel_ul').css({'left' : '-210px'});
 //         }); 
 //     });
-    
+
 //     //when user clicks the image for sliding left
 //     $('#left_scroll img').click(function(){
 
 //         var item_width = $('#carousel_ul li').outerWidth() + 10;
-        
+
 //         /* same as for sliding right except that it's current left indent + the item width (for the sliding right it's - item_width) */
 //         var left_indent = parseInt($('#carousel_ul').css('left')) + item_width;
-        
+
 //         $('#carousel_ul:not(:animated)').animate({'left' : left_indent},500,function(){    
 
 //             /* when sliding to left we are moving the last item before the first list item */            
@@ -196,7 +258,7 @@ bindElementToAtopOpening('#coderwall-social', coderwall_data);
 //             /* and again, when we make that change we are setting the left indent of our unordered list to the default -210px */
 //             $('#carousel_ul').css({'left' : '-210px'});
 //         });
-        
-        
+
+
 //     });
 // });
